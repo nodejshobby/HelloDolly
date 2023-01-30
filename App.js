2229693b0fa6d63e04044af70e6b75789ed66420
nodejshobby <nodejshobby@gmail.com>
2023-01-30 21:37:22 +0100

    HelloDolly will be a good reference to my react native journey

--- App.js
+++ App.js
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.Heading}>Hello Dolly</Text>
+      <Text style={styles.SubHeading}>A simple dummy text mobile application built with love ❤</Text>
+      <Text style={styles.Footer}>Copyright &copy; 2023</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +15,28 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'orange',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  Heading: {
+    color: 'white',
+    fontSize: 40,
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
+  SubHeading: {
+    color: '#000000',
+    fontSize: 15,
+    fontWeight: '600',
+    marginTop: 15,
+    paddingHorizontal: 'auto',
+    width: 300,
+    textAlign: 'center',
+  },
+  Footer: {
+    fontWeight: '500',
+    position: 'absolute',
+    bottom: '10%',
+  }
 });
